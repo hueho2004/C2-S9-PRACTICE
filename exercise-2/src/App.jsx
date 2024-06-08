6dd47b2a9261f87b219e0bedceaa2985ef6be686
hothihue <hue.ho25@student.passerellesnumeriques.org>
2024-06-08 10:47:39 +0700

    finish bai 2

--- exercise-2/src/App.jsx
+++ exercise-2/src/App.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 function App() {
-  /* You will need to use a boolean state to keep the value of the text entered */
-
+  const [inputText, setInputText] = useState("");
+    /* You will need to use a boolean state to keep the value of the text entered */
   /* You will need a function to handle a key pressed on the first input and update the state*/
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
   return (
     <main>
       <h1>Upper Case Converter</h1>
 
-      <label>Enter any text !</label>
-      <input />
+      <label htmlFor="input-text">Enter any text:</label>
+      <input id="input-text" type="text" onChange={handleInputChange} />
 
       <p>
-        <label>Here is the text in upper case</label>
-
-        {/* This input will need to display the text entered in lower case */}
-        <input disabled />
+          {/* This input will need to display the text entered in lower case */}
+        <label htmlFor="output-text">Here is the text in upper case:</label>
+        <input id="output-text" type="text" disabled value={inputText.toUpperCase()} />
       </p>
     </main>
   );
 }
-
 export default App;
